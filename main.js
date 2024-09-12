0a27ff82807ce87825c4dcc4b85b2d0173738345
Maxi <maxifranco@mail.com>
2024-09-12 04:18:29 -0300

    Add content 2.0

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let array = []
 let section = document.querySelector('section')
 
 for (let i = 1; i <= 9; i++){
+    let precio = Math.round(Math.random() * (100000 - 50000) + 50000);
     array.push(`
          <div class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Auto"> <div class="card-body">
-    <h5 class="card-title">Card ${i}</h5> </div> </div>
+    <h5 class="card-title">Auto caro N° ${i}</h5> <h4>Precio: $${precio}</h4> <a href="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" target="_blank">Ver Auto caro</a></div> </div>
 `)
-
 }
 
 section.innerHTML = array.join("");
