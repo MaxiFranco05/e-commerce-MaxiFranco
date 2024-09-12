7bafc1980ad8b93152370e4fb79fae4effb0555a
Maxi <maxifranco@mail.com>
2024-09-12 03:28:33 -0300

    Add content 1.0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector("h1").innerText = 'Productos'
 let array = []
 let section = document.querySelector('section')
 
-for (let i = 1; 1 <= 9; i++){
+for (let i = 1; i <= 9; i++){
     array.push(`
          <div class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Auto"> <div class="card-body">
     <h5 class="card-title">Card ${i}</h5> </div>
