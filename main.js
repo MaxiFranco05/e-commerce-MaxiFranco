46a2a28e9f1d1b36cfac859528d96d2ac065c3a8
Maxi <maxifranco@mail.com>
2024-09-12 20:32:49 -0300

    Add content 2.1

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+function cards (contain, cont){
+    let precio = Math.round(Math.random() * (100000 - 50000) + 50000);
+    contain.push(`  <div class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${cont}.jpg" class="card-img-top" alt="Auto"> <div class="card-body"><h5 class="card-title">Auto caro N° ${cont}</h5> <h4>Precio: $${precio}</h4> <a href="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${cont}.jpg" target="_blank">Ver más</a></div></div>  `)
+}
+
 console.log("Hello World!")
 document.querySelector("h1").innerText = 'Productos'
 let array = []
 let section = document.querySelector('section')
 
 for (let i = 1; i <= 9; i++){
-    let precio = Math.round(Math.random() * (100000 - 50000) + 50000);
-    array.push(`
-         <div class="card"> <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Auto"> <div class="card-body">
-    <h5 class="card-title">Auto caro N° ${i}</h5> <h4>Precio: $${precio}</h4> <a href="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" target="_blank">Ver Auto caro</a></div> </div>
-`)
+    cards(array, i)
 }
 
 section.innerHTML = array.join("");
